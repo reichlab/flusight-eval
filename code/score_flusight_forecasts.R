95778cb0fbafe3c5d185f412ff740406c50fe754
mzorn-58 <66443989+mzorn-58@users.noreply.github.com>
2024-01-08 17:04:44 -0500

    Add files via upload

--- code/score_flusight_forecasts.R
+++ code/score_flusight_forecasts.R
@@ -102,7 +102,7 @@ scores_log <- log_scores |>
 head(scores_log)
 
 #write rda 
-setwd("~/github/flu-hosp-forecasts/reports")
+setwd("~/github/flusight-eval/reports")
 save(raw_data, file = "raw_data.rda")
 save(log_data, file = "log_data.rda")
 save(raw_scores, file = "raw_scores.rda")
